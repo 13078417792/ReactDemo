4500d130315460bb066f519cc16c637c911f323c
zhouchijian <397201698@qq.com>
2019-01-01 16:38:14 +0800

    修改IP

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ module.exports = function override(config, env) {
     //do stuff with the webpack config...
 
     config = rewireLess(config, env)
-    process.env.HOST = '192.168.1.9'
+    process.env.HOST = '0.0.0.0'
     process.env.PORT = 8889
     // process.env.OPEN = true
     return config

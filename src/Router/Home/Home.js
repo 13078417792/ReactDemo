ce38d9891c012048e2413785eb90e343820f57c1
zhouchijian <397201698@qq.com>
2019-01-02 21:19:31 +0800

    'update'

--- src/Router/Home/Home.js
+++ src/Router/Home/Home.js
@@ -2,6 +2,9 @@ import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 import {withRouter} from 'react-router'
 import './HomeStyle.less'
+// import Layout from '../../components/Layout/Layout'
+import HeaderSearch from '../../components/HeaderSearch/HeaderSearch'
+import cs from 'classnames'
 
 class Home extends Component{
 
@@ -9,12 +12,46 @@ class Home extends Component{
         match: PropTypes.object.isRequired,
         location: PropTypes.object.isRequired,
         history: PropTypes.object.isRequired
-    };
+    }
+
+    // static
+    static contextTypes = { theme: PropTypes.object }
+
+    constructor(props){
+        super(props)
+    }
 
     render(){
+        const tools = [
+            {
+                pic:'/pic/uwp-bg-2.jpg',
+                name:'图片转Base64',
+                tag:'pic-to-base64',
+                url:'/'
+            }
+        ]
+
+        const {theme} = this.context
         return (
             <div className="Home">
-                Home
+                <HeaderSearch></HeaderSearch>
+
+                <div className="tools-list">
+
+                    {
+                        tools.map((el,key)=>{
+                            return (
+
+                                <div className={cs('tool-item',el.tag)} style={{
+                                    background:theme.acrylicTexture40.background
+                                }}>
+
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
             </div>
         )
     }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,88 @@
 import React,{Component} from 'react'
+import cs from 'classnames'
 import './LayoutStyle.less'
+import PropTypes from 'prop-types'
+// import NavigationView from "react-uwp/NavigationView";
+import IconButton from "react-uwp/IconButton";
+
 
 export default class Layout extends Component{
 
-    render(){
-        return (
-            <div>
+    static contextTypes = { theme: PropTypes.object }
 
+    constructor(props){
+        super(props)
+        this.state = {
+            mobileLeftSide:false
+        }
+    }
+
+    toggleMobileLeftSide(){
+        this.setState(state => ({mobileLeftSide:!state.mobileLeftSide}) )
+    }
+
+    render(){
+        const {theme} = this.context
+        console.log(this.context,theme.acrylicTexture40.background)
+        const buttonStyle = {
+            height:'3em',
+            width:'3em',
+            lineHeight:'3em',
+            fontSize:'1em'
+        }
+        return (
+            <div className="Layout" style={{
+                background:theme.acrylicTexture40.background
+            }}>
+
+                {/* 移动端头部 */}
+                <div className="mobile-header" style={{
+                    // background:theme.acrylicTexture60.background
+                    background:'rgba(0,0,0,.1)'
+                }}>
+
+                    <div className="menu-rt-btn">
+                        <div className="menu-rt-btn-scroll-container" style={{
+                            transform:`translateX(${this.state.mobileLeftSide?'-100%':0})`
+                        }}>
+                            <IconButton style={buttonStyle} hoverStyle={{
+                                backgroundColor:'rgba(0,0,0,0)'
+                            }} activeStyle={{
+                                backgroundColor:'rgba(0,0,0,0)'
+                            }} onClick={this.toggleMobileLeftSide.bind(this)}>
+                                GlobalNavButton
+                            </IconButton>
+                            <IconButton style={buttonStyle} hoverStyle={{
+                                backgroundColor:'transparent'
+                            }} activeStyle={{
+                                backgroundColor:'transparent'
+                            }} onClick={this.toggleMobileLeftSide.bind(this)}>
+                                BackBttnArrow42Legacy
+                            </IconButton>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div id="content">
+
+
+                    {/*PC默认显示 移动端默认隐藏*/}
+                    <div id="left-side-bar" className={cs({expand:this.state.mobileLeftSide})} style={{
+                        background:theme.acrylicTexture60.background
+                    }}>
+
+                    </div>
+
+                    <div id="right-content-wrap">
+                        <div className="right-content">
+                            {this.props.children}
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }

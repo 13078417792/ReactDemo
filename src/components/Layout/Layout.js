420dfcbb2330760161b6b18d677932948b6c84ef
zhouchijian <397201698@qq.com>
2019-01-02 13:16:34 +0800

    布局组件

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import './LayoutStyle.less'
+
+export default class Layout extends Component{
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}

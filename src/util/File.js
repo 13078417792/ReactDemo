13edeba49338ae98c94ceb2e9e79e04a701c232b
zhouchijian <397201698@qq.com>
2019-01-08 16:14:19 +0800

    文件操作

--- src/util/File.js
+++ src/util/File.js
@@ -17,6 +17,42 @@ File.prototype.toBase64 = function(){
     })
 }
 
+File.prototype.toArrayBuffer = function(){
+    return new Promise((resolve,reject)=>{
+        // console.log(this)
+
+        const file = this
+        const reader = new FileReader()
+        reader.onload = function(){
+            resolve(reader.result)
+        }
+
+        reader.onerror = function(){
+            reject(reader.error)
+        }
+
+        reader.readAsArrayBuffer(file)
+    })
+}
+
+File.prototype.toBinary = function(){
+    return new Promise((resolve,reject)=>{
+        // console.log(this)
+
+        const file = this
+        const reader = new FileReader()
+        reader.onload = function(){
+            resolve(reader.result)
+        }
+
+        reader.onerror = function(){
+            reject(reader.error)
+        }
+
+        reader.readAsText(file)
+    })
+}
+
 let FileHelper = {
 
     getDataview(file){
@@ -72,6 +108,54 @@ let FileHelper = {
             }
             file.toBase64().then(resolve).catch(reject)
         })
+    },
+
+    toArrayBuffer(file){
+        return new Promise((resolve,reject)=> {
+            if (!helper.isFile(file)) {
+                reject('不是文件类型');
+                return;
+            }
+            file.toArrayBuffer().then(resolve).catch(reject)
+        })
+    },
+
+    toBinary(file){
+        return new Promise((resolve,reject)=> {
+            if (!helper.isFile(file)) {
+                reject('不是文件类型');
+                return;
+            }
+            file.toBinary().then(resolve).catch(reject)
+        })
+    },
+
+    /**
+     *
+     * @param DataView view
+     * @param int start
+     * @param int length
+     * @returns {Array}
+     */
+    getFileDataView(view,start,length){
+        if(toString.call(view)!=='[object DataView]') return [];
+        if(!start) start = 0
+        if(!length) length = view.byteLength>0?view.byteLength:0
+        if(!helper.isInt(start) || !helper.isInt(length) || start<0) return []
+        let result = []
+
+        // console.log(start,length)
+        for(let i=start;i<start+length;i++){
+            try{
+                result.push(view.getUint8(i))
+            }catch(e){
+                console.log(i)
+                console.error(e)
+                return;
+            }
+
+        }
+        return result
     }
 }
 

c7946df661af25c4ecc1b6f147eac41c281d5e6d
zhouchijian <397201698@qq.com>
2019-01-13 17:02:07 +0800

    页面加载时显示的动画

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,84 @@
+import React,{Component} from 'react'
+import './LoadingStyle.less'
+import cs from 'classnames'
+
+class Loading extends Component{
+
+    constructor(props){
+        super(props)
+        this.container = React.createRef()
+        this.parent = React.createRef()
+        this.child = React.createRef()
+        const size = 100
+        const border = 5
+        this.state = {
+            width:0,
+            height:0,
+            size,
+            border,
+            radius:(size+border*2)/2
+        }
+    }
+
+
+
+    componentDidMount(){
+        this.setState({
+            width:this.container.clientWidth,
+            height:this.container.clientHeight
+        })
+        this.drawOuterCircle.call(this)
+        this.drawInnerCircle.call(this)
+    }
+
+    // 外圆
+    drawOuterCircle(){
+        const {state:{size,border,radius},parent:{current:parent}} = this
+
+        const ctx = parent.getContext('2d')
+        ctx.beginPath()
+        ctx.strokeStyle = '#e2e2e2'
+        ctx.lineWidth = 5
+        ctx.arc(radius,radius,size/2,0,Math.PI * 2)
+        ctx.stroke()
+        ctx.closePath()
+    }
+
+    // 内圆圆弧
+    drawInnerCircle(){
+        const {state:{size,border,radius},child:{current:child}} = this
+        const ctx = child.getContext('2d')
+        ctx.beginPath()
+        ctx.strokeStyle = '#b0b0b0'
+        ctx.lineWidth = 5
+        ctx.arc(radius,radius,size/2,Math.PI/180*-20,Math.PI/180*-90,true)
+        ctx.stroke()
+        ctx.closePath()
+    }
+
+    render(){
+        const {state:{size,border}} = this
+        return (
+            <div className="loading-middleware">
+
+                <div className="container" ref={this.container} style={{
+                    width:`${size+border*2}px`,
+                    height:`${size+border*2}px`
+                }}>
+
+                    <canvas ref={this.parent} className={"parent"} width={`${size+border*2}px`} height={`${size+border*2}px`}>
+
+                    </canvas>
+
+                    <canvas ref={this.child} className={"child"} width={`${size+border*2}px`} height={`${size+border*2}px`}>
+
+                    </canvas>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Loading;

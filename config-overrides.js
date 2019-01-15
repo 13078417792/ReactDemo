249344df0e199dd1202389502348c5f1d4f0b639
zhouchijian <397201698@qq.com>
2019-01-15 18:18:23 +0800

    web-worker支持

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,29 @@
 const rewireLess = require('react-app-rewire-less');
+const {injectBabelPlugin,getLoader} = require('react-app-rewired')
+const rewired = require('react-app-rewired')
 
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
 
     config = rewireLess(config, env)
+    config = injectBabelPlugin(['@babel/plugin-proposal-decorators', { "legacy": true }], config)
     process.env.HOST = '0.0.0.0'
     process.env.PORT = 8889
+
+    config.module.rules = config.module.rules.map(rule=>{
+        // console.log(rule)
+        if(rule && Array.isArray(rule.oneOf)){
+
+            rule.oneOf.unshift({
+                test:/\.worker\.js$/,
+                exclude: /node_modules/,
+                use:{ loader: 'worker-loader',options: { name: 'WorkerName.[hash].js',publicPath:'/src/workers/',inline: true } },
+            })
+        }
+        return rule
+    })
+    config.output.globalObject = 'this'
+    // throw 123;
     // process.env.OPEN = true
     return config
-  }
+}

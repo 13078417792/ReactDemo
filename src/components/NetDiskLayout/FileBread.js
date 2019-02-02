c7b4ef06e943936920d92712583d404bcb1b3908
zhouchijian <397201698@qq.com>
2019-02-03 01:13:26 +0800

    渲染逻辑优化

---
+++ src/components/NetDiskLayout/FileBread.js
@@ -0,0 +1,82 @@
+import React,{Component} from 'react'
+import './FileBreadStyle.less'
+import {observer,inject} from 'mobx-react'
+import {Link} from 'react-router-dom'
+
+@inject("stores")
+@observer
+class FileBread extends Component{
+
+
+
+    constructor(props){
+        super(props)
+    }
+
+    path(){
+        const {stores:{DiskStore}} = this.props
+        if(DiskStore.path.length===0) return null;
+        return (
+            <div className="inlb path">
+
+                {
+                    DiskStore.path.map((el,index)=>{
+                        if(index===DiskStore.path.length-1){
+                            return (
+                                <span key={index}>
+                                    {el.name}
+                                </span>
+                            )
+                        }
+                        return (
+                            <Link key={index} to={`/disk/content/${el.id}`}>
+                                {el.name}
+                            </Link>
+                        )
+                    })
+                }
+
+            </div>
+        )
+    }
+
+    render(){
+        const {props} = this
+        const {stores:{DiskStore}} = props
+        const Path = this.path.bind(this)
+        return (
+            <div className="file-bread">
+
+                {
+                    DiskStore.path>0?(
+                        <div className="inlb goback">
+                            <Link to={`/disk/content/${DiskStore.path.length-2>=0?`/${DiskStore.path[DiskStore.path.length-2].id}`:''}`}>
+                                返回上一层
+                            </Link>
+                        </div>
+                    ):null
+                }
+
+
+                <div className="inlb all">
+                    {
+                        DiskStore.folderId>0?(
+                            <Link to={`/disk/content`}>
+                                全部文件
+                            </Link>
+                            ):(
+                            <span>
+                                全部文件
+                            </span>
+                        )
+                    }
+                </div>
+
+                <Path/>
+
+            </div>
+        )
+    }
+}
+
+export default FileBread

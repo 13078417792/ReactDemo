256674f59532919bd9fc4b5e1bfd78344c157b1b
zhouchijian <397201698@qq.com>
2019-02-03 01:08:20 +0800

    手动设置登录状态

--- src/Store/AccountStatus.js
+++ src/Store/AccountStatus.js
@@ -5,10 +5,13 @@ const {observable,action} = require('mobx')
 // 定时检查登录状态的时间间隔
 const CHECK_AUTH_TIME = 1000*60*5
 
+// const CHECK_AUTH_TIME = 5000
+
 export default class AccountStatus {
 
     @observable isLogin:boolean = false;
     @observable initCheckingLogin:boolean = true;
+    @observable checkingAuth:boolean = false;
     @observable AuthID:string = '';
     @observable Token:string = '';
 
@@ -18,9 +21,7 @@ export default class AccountStatus {
 
         this.initAuthAndToken()
 
-        setTimeout(()=>{
-            this.checkAuth()
-        },2000)
+        this.checkAuth()
         this.onTimeCheckAuth()
         this.AuthID = Auth.getAuthID()
         this.Token = Auth.getCommonToken()
@@ -61,24 +62,48 @@ export default class AccountStatus {
     }
 
     checkAuth(){
+        this.checkingAuth = true
+        const oldStatus = this.isLogin
         Auth.checkAuthID().then(result=>{
             this.isLogin = !!result.success
             if(!result.success){
                 Auth.clearAuthID()
+                this.setStatus(false)
+                if(oldStatus){
+                    window.location.href = '/'
+                }
             }
+            this.checkingAuth = false
             this.initCheckingLogin = false
         }).catch(err=>{
             this.isLogin = false
             this.AuthID = ''
             Auth.clearAuthID()
+            this.setStatus(false)
             console.error(err)
+            this.checkingAuth = false
             this.initCheckingLogin = false
+            if(oldStatus){
+                window.location.href = '/'
+            }
         })
     }
 
     @action SignIn(){
         this.isLogin = true
         this.AuthID = Auth.getAuthID()
+    }
+
+    @action setStatus(status:boolean){
+        this.isLogin = !!status
+        this.AuthID = null
+    }
+
+    @action toggleStatus(){
+        this.isLogin = !this.isLogin
+        if(!this.isLogin){
+            this.AuthID = null
+        }
     }
 
 

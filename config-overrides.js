aa6c06658131c34cb33bac67e209904a2a59e8ba
zhouchijian <397201698@qq.com>
2019-02-08 22:40:13 +0800

    生产环境禁用console

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,6 @@
 const rewireLess = require('react-app-rewire-less');
-const {injectBabelPlugin,getLoader} = require('react-app-rewired')
-const rewired = require('react-app-rewired')
-
+const {injectBabelPlugin} = require('react-app-rewired')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const path = require('path')
 
@@ -9,25 +8,44 @@ module.exports = function override(config, env) {
     //do stuff with the webpack config...
 
     config = rewireLess(config, env)
-    config = injectBabelPlugin(['@babel/plugin-proposal-decorators', { "legacy": true }], config)
+    config = injectBabelPlugin(['@babel/plugin-proposal-decorators', {
+        legacy: true
+    }], config)
+
+    if(env==='production'){
+        config.plugins.push(new UglifyJsPlugin({
+            uglifyOptions: {
+                compress: {
+                    warnings: false,
+                    drop_debugger: true,
+                    drop_console: true
+                }
+            }
+        }))
+    }
+
+
     process.env.HOST = '0.0.0.0'
     process.env.PORT = 8889
 
-    config.module.rules = config.module.rules.map(rule=>{
+
+    config.module.rules = config.module.rules.map(rule => {
         // console.log(rule)
-        if(rule && Array.isArray(rule.oneOf)){
+        if (rule && Array.isArray(rule.oneOf)) {
 
             rule.oneOf.unshift({
-                test:/\.worker\.js$/,
+                test: /\.worker\.js$/,
                 exclude: /node_modules/,
-                use:{ loader: 'worker-loader',options: { name: 'WorkerName.[hash].js',publicPath:'/src/workers/',inline: true } },
+                use: {
+                    loader: 'worker-loader',
+                    options: {name: 'Worker.[hash].js', publicPath: '/src/workers/', inline: true}
+                },
             })
         }
         return rule
     })
-    config.output.globalObject = 'this'
+    // config.output.globalObject = 'this'
 
-    console.log(config.resolve)
     config.resolve.alias['@src'] = path.resolve('src')
     config.resolve.alias['@components'] = path.resolve('src/components')
     config.resolve.alias['@store'] = path.resolve('src/Store')
@@ -35,8 +53,9 @@ module.exports = function override(config, env) {
     config.resolve.alias['@workers'] = path.resolve('src/workers')
     config.resolve.alias['@router'] = path.resolve('src/Router')
     config.resolve.alias['@drawer'] = path.resolve('src/Drawer')
+    // console.log(config)
+    // throw 1233;
+    // config.output.publicPath = ""
 
-    // throw 123;
-    // process.env.OPEN = true
     return config
 }

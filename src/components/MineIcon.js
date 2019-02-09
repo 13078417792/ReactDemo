c646eddca493c4192386951dce87e57ebc835658
zhouchijian <397201698@qq.com>
2019-02-09 14:03:20 +0800

    新增网易云图标

--- src/components/MineIcon.js
+++ src/components/MineIcon.js
@@ -2,7 +2,7 @@ import {Icon} from 'antd'
 
 
 const MineIcon = Icon.createFromIconfontCN({
-    scriptUrl:'//at.alicdn.com/t/font_1013439_4n60zl07bzr.js'
+    scriptUrl:'//at.alicdn.com/t/font_1013439_lpqhyttjnha.js'
 })
 
 export default MineIcon

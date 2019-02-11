1ab38ed8a63188c728ffdeb9138568724905b906
zhouchijian <397201698@qq.com>
2019-02-11 22:14:18 +0800

    更改电台图标

--- src/components/MineIcon.js
+++ src/components/MineIcon.js
@@ -2,7 +2,7 @@ import {Icon} from 'antd'
 
 
 const MineIcon = Icon.createFromIconfontCN({
-    scriptUrl:'//at.alicdn.com/t/font_1013439_6vm3tv0ic5.js'
+    scriptUrl:'//at.alicdn.com/t/font_1013439_oweje9j1gw.js'
 })
 
 export default MineIcon

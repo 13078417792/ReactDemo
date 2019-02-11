02031b9f2788b02b6aa736f9bc1d89e9d64727fe
zhouchijian <397201698@qq.com>
2019-02-11 18:19:33 +0800

    添加图标:newest

--- src/components/MineIcon.js
+++ src/components/MineIcon.js
@@ -2,7 +2,7 @@ import {Icon} from 'antd'
 
 
 const MineIcon = Icon.createFromIconfontCN({
-    scriptUrl:'//at.alicdn.com/t/font_1013439_jwdzckl3tp.js'
+    scriptUrl:'//at.alicdn.com/t/font_1013439_6vm3tv0ic5.js'
 })
 
 export default MineIcon

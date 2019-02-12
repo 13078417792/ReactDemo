9036739f4e49a464bee3daec03c456c45207316a
zhouchijian <397201698@qq.com>
2019-02-12 18:19:22 +0800

    新增播放图标

--- src/components/MineIcon.js
+++ src/components/MineIcon.js
@@ -2,7 +2,7 @@ import {Icon} from 'antd'
 
 
 const MineIcon = Icon.createFromIconfontCN({
-    scriptUrl:'//at.alicdn.com/t/font_1013439_oweje9j1gw.js'
+    scriptUrl:'//at.alicdn.com/t/font_1013439_s6il1tof0k.js'
 })
 
 export default MineIcon

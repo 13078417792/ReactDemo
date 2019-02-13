d0666723cb60d9abaa20f326de155daeb72c6b5c
zhouchijian <397201698@qq.com>
2019-02-13 13:06:00 +0800

    颜色

--- src/Store/UI.js
+++ src/Store/UI.js
@@ -4,7 +4,45 @@ const {observable,action} = require('mobx')
 
 export default class UI{
 
-    @observable wy_toggle_button:boolean = true
+    // element-ui推荐颜色
+    color = {
+        blue:'#409EFF',
+        success:'#67C23A',
+        warning:'#E6A23C',
+        danger:'#F56C6C',
+        info:'#909399',
+    }
+
+    // layui推荐颜色
+    layuiColor:{
+        button:'#009688',
+        checked:'#5FB878',
+        nav:'#393D49',
+        brightBlue:'#1E9FFF',
+        message:'#FFB800',
+        focus:'#FF5722',
+        text:'#01AAED',
+        side:'#2F4056'
+    }
+
+    // 字体颜色
+    fontColor:{
+        main:'#303133',
+        normal:'#606266',
+        second:'#909399',
+        placeholder:'#C0C4CC'
+    }
+
+    // 边框颜色
+    border:{
+        first:'#DCDFE6',
+        second:'#E4E7ED',
+        third:'#EBEEF5',
+        four:'#F2F6FC'
+    }
+
+    @observable wy_music_side:boolean = false
+    @observable wy_music_side_only_icon = false
 
     @action toggle(name:string){
         if(!this.hasOwnProperty(name)) return false

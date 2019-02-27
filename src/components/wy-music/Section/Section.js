0bbf68437c9f25bb48617431db50ebecf1b56a84
zhouchijian <397201698@qq.com>
2019-02-27 15:52:58 +0800

    后缀

--- src/components/wy-music/Section/Section.js
+++ src/components/wy-music/Section/Section.js
@@ -1,41 +1,61 @@
 import './SectionStyle.less'
-import React,{Component,cloneElement} from 'react'
+import React, { Component, cloneElement, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import cs from 'classnames'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-class Section extends Component{
+class Section extends Component {
 
     static propTypes = {
-        title:PropTypes.string.isRequired,
-        icon:PropTypes.element.isRequired,
-        more:PropTypes.string,
-        className:PropTypes.string,
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.element,
+        more: PropTypes.string,
+        className: PropTypes.string,
+        suffix: PropTypes.element,
+        color: PropTypes.string
     }
 
     static defaultProps = {
-        more:'',
-        className:''
+        more: '',
+        className: '',
+        icon: null,
+        suffix: null,
+        color: null
     }
 
     Icon = (props) => {
-        return cloneElement(this.props.icon,props)
+        const { icon } = this.props
+        return icon ? cloneElement(icon, props) : null
     }
 
-
-    render(){
-        const {props:{title,className,more,children},Icon} = this
+    RightPart = props => {
+        const { suffix, more } = this.props
         return (
-            <div className={cs('music-section',className)}>
+            <Fragment>
 
-                <div className="description-title">
+                <div className="suffix">
+                    {more ? <Link className="more" to={more}>更多></Link> : null}
+                    {suffix}
+                </div>
+
+            </Fragment>
+        )
+    }
+
+    render() {
+        const { props: { title, className, more, children, color }, Icon, RightPart } = this
+        return (
+            <div className={cs('music-section', className)}>
+
+                <div className="description-title" style={{
+                    color, borderColor: color
+                }}>
+
                     <Icon className="icon" />
                     <span>{title}</span>
 
 
-                    {
-                        more?<Link className="more" to={more}>更多></Link>:null
-                    }
+                    <RightPart />
 
                 </div>
 

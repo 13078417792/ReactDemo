d04ad04bdd9ed92cfcf79bf36134277d6f7f0941
zhouchijian <397201698@qq.com>
2019-02-27 11:50:42 +0800

    调用网易红加载动画组件

--- src/components/wy-music/Comment/Comment.js
+++ src/components/wy-music/Comment/Comment.js
@@ -1,125 +1,124 @@
-import React,{Component,Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 import cs from 'classnames'
 import PropTypes from 'prop-types'
 import API from '@util/CloudMusicAPI'
-import {computed} from 'mobx'
-import {observer,inject} from 'mobx-react'
+import { computed } from 'mobx'
+import { observer, inject } from 'mobx-react'
 import './CommentStyle.less'
-import {message,Avatar,Icon,Pagination} from 'antd'
+import { message, Avatar, Icon, Pagination } from 'antd'
 import Helper from '@util/Helper'
-import DoubleColumn from '@components/DoubleColumn/DoubleColumn'
 import dayjs from 'dayjs'
-import Loading from '@components/Loading/Loading'
+import Loading from '@components/wy-music/Loading'
 
 @inject('stores') @observer
 class Comment extends Component {
 
     static propTypes = {
-        type:PropTypes.oneOf([
-            'playlist','music'
+        type: PropTypes.oneOf([
+            'playlist', 'music'
         ]).isRequired,
-        id:PropTypes.number.isRequired,
-        api:PropTypes.string,
-        params:PropTypes.object,
-        count:PropTypes.number,
-        onFetch:PropTypes.func
+        id: PropTypes.number.isRequired,
+        api: PropTypes.string,
+        params: PropTypes.object,
+        count: PropTypes.number,
+        onFetch: PropTypes.func
     }
 
     static defaultProps = {
-        api:'',
-        id:0,
-        params:{},
-        count:20,
-        onFetch:function(){}
+        api: '',
+        id: 0,
+        params: {},
+        count: 20,
+        onFetch: function () { }
     }
 
     state = {
-        current_page:1,
-        page:1,
-        hotComments:[],
-        comments:[],
-        total:0,
-        fetching:true,
+        current_page: 1,
+        page: 1,
+        hotComments: [],
+        comments: [],
+        total: 0,
+        fetching: true,
     }
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.fetch().then(({hotComments,comments,total})=>{
-            const page = Math.ceil(total/props.count)
+        this.fetch().then(({ hotComments, comments, total }) => {
+            const page = Math.ceil(total / props.count)
             this.setState({
                 page,
-                hotComments,comments,total,
-                fetching:false
+                hotComments, comments, total,
+                fetching: false
             })
-            props.onFetch({hotComments,comments,total})
-        }).catch(err=>{
+            props.onFetch({ hotComments, comments, total })
+        }).catch(err => {
             message.error(Helper.handleErrorMsg(err))
             this.setState({
-                fetching:false
+                fetching: false
             })
         })
     }
 
-    @computed get params(){
-        let {params,id,count} = this.props
+    @computed get params() {
+        let { params, id, count } = this.props
         params.id = id
         params.limit = count
         return params
     }
 
-    @computed get api(){
-        if(this.props.api) return this.props.api
+    @computed get api() {
+        if (this.props.api) return this.props.api
         const api = {
-            playlist:'COMMENT_PLAYLIST',
-            music:'COMMENT_MUSIC'
+            playlist: 'COMMENT_PLAYLIST',
+            music: 'COMMENT_MUSIC'
         }
         return api[this.props.type]
     }
 
-    fetch(){
-        const {params,api,props:{count},state:{current_page}} = this
-        let query = Object.assign({},params,{
-            offset:(current_page-1)*count
+    fetch() {
+        const { params, api, props: { count }, state: { current_page } } = this
+        let query = Object.assign({}, params, {
+            offset: (current_page - 1) * count
         })
-        return API.fetch(api,query).then(result=>{
-            if(result.code!==200) return Promise.reject('请求失败')
-            const {hotComments,comments,total} = result
+        return API.fetch(api, query).then(result => {
+            if (result.code !== 200) return Promise.reject('请求失败')
+            const { hotComments, comments, total } = result
             return Promise.resolve({
-                hotComments:Array.isArray(hotComments)?hotComments:[],
+                hotComments: Array.isArray(hotComments) ? hotComments : [],
                 comments,
                 total
             })
-        }).catch(err=>{
+        }).catch(err => {
             return Promise.reject(err);
         })
     }
 
     Content = props => {
-        const {Content} = this
-        const {reply,user,content,isReply} = props
+        const { Content } = this
+        const { reply, user, content, isReply } = props
         let hasReply = false
         return (
             <span className={cs('content-wrap')}>
                 <span className="user" >
-                    {!!isReply?'@':''}{user}
+                    {!!isReply ? '@' : ''}{user}
                 </span>:&nbsp;
 
                 {
-                    reply?(
+                    reply ? (
                         <Fragment>
                             回复
                             <span className="user" >
                                 @{reply.user.nickname}
                             </span>:&nbsp;
                         </Fragment>
-                    ):null
+                    ) : null
                 }
 
                 <span className="content">
                     {content}
                 </span>
                 {
-                    reply?(
+                    reply ? (
                         <div className="child-content">
                             <Content
                                 isReply={true}
@@ -127,7 +126,7 @@ class Comment extends Component {
                                 content={reply.content}
                             />
                         </div>
-                    ):null
+                    ) : null
                 }
 
 
@@ -136,7 +135,7 @@ class Comment extends Component {
     }
 
     CommentItemBottom = props => {
-        const {time,likedCount,liked} = props
+        const { time, likedCount, liked } = props
         return (
             <div className="bottom">
 
@@ -148,8 +147,8 @@ class Comment extends Component {
 
                     {/* 点赞量 */}
                     <span className="liked">
-                        <Icon className={cs('icon-like',{
-                            'icon-liked':liked
+                        <Icon className={cs('icon-like', {
+                            'icon-liked': liked
                         })} type="like" />（{likedCount}）
                     </span>
 
@@ -160,8 +159,8 @@ class Comment extends Component {
     }
 
     CommentItem = props => {
-        const {Content,CommentItemBottom} = this
-        const {user,content,time,likedCount,liked,avatar,reply} = props
+        const { Content, CommentItemBottom } = this
+        const { user, content, time, likedCount, liked, avatar, reply } = props
         return (
             <section className="comment-item cl">
 
@@ -169,7 +168,7 @@ class Comment extends Component {
 
                 <div className="right-part">
                     <Content
-                        reply={Array.isArray(reply) && reply.length?reply[0]:null}
+                        reply={Array.isArray(reply) && reply.length ? reply[0] : null}
                         user={user}
                         content={content}
                     />
@@ -183,9 +182,9 @@ class Comment extends Component {
     }
 
     Part = props => {
-        const {CommentItem} = this
-        let {comments} = props
-        if(!Array.isArray(comments)) comments = []
+        const { CommentItem } = this
+        let { comments } = props
+        if (!Array.isArray(comments)) comments = []
         return (
             <div className="comment-part">
 
@@ -195,7 +194,7 @@ class Comment extends Component {
                 <div className="content">
 
                     {
-                        comments.map((el,index)=>{
+                        comments.map((el, index) => {
                             return (
                                 <CommentItem
                                     key={index}
@@ -219,74 +218,74 @@ class Comment extends Component {
 
     onChangePage = page => {
         this.setState({
-            current_page:page,
-            fetching:true
-        },()=>{
-            this.fetch().then(({hotComments,comments,total})=>{
+            current_page: page,
+            fetching: true
+        }, () => {
+            this.fetch().then(({ hotComments, comments, total }) => {
                 this.setState({
                     page,
-                    hotComments,comments,total,
-                    fetching:false
+                    hotComments, comments, total,
+                    fetching: false
                 })
-                this.props.onFetch({hotComments,comments,total})
+                this.props.onFetch({ hotComments, comments, total })
             })
         })
 
     }
 
     All = () => {
-        const {state,Part,props} = this
-        const {hotComments:hot,comments,total,current_page} = state
-        const {count} = props
+        const { state, Part, props } = this
+        const { hotComments: hot, comments, total, current_page } = state
+        const { count } = props
 
         return (
             <Fragment>
                 {
-                    hot.length?(
+                    hot.length ? (
                         <Part
                             title="精彩评论"
                             comments={hot}
                         />
-                    ):null
+                    ) : null
                 }
 
                 {
-                    comments.length?(
+                    comments.length ? (
                         <Part title="最新评论" comments={comments} />
-                    ):null
+                    ) : null
                 }
 
                 {
-                    total && total>count ?(
+                    total && total > count ? (
                         <Pagination className={"page"}
-                                    total={total}
-                                    pageSize={count}
-                                    size="small"
-                                    showQuickJumper
-                                    hideOnSinglePage={true}
-                                    current={current_page}
-                                    onChange={this.onChangePage}
+                            total={total}
+                            pageSize={count}
+                            size="small"
+                            showQuickJumper
+                            hideOnSinglePage={true}
+                            current={current_page}
+                            onChange={this.onChangePage}
                         />
-                    ):null
+                    ) : null
                 }
             </Fragment>
         )
     }
 
-    render(){
-        const {state,Part,All} = this
-        const {fetching} = state
+    render() {
+        const { state, Part, All } = this
+        const { fetching } = state
         return (
             <div className="comment-component">
 
                 {
-                    fetching?(
-                        <Loading size={35} border={2} color={'#e4bbbb'} sec_color={'#C72E2E'} style={{
-                            position:'absolute',
-                            width:'100%',
-                            height:'300px'
+                    fetching ? (
+                        <Loading position="absolute" style={{
+                            width: '100%',
+                            height: '300px'
                         }} />
-                    ):<All/>
+
+                    ) : <All />
                 }
 
 
